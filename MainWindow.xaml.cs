66bbed589155653c20be3f4d33345f6d92aa452c
Mustafa Can Yucel <m.can.yucel.1@gmail.com>
2022-11-25 11:56:35 +0300

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SignalDataPicker
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public List<Record> Records { get; set; } = new List<Record>();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void btnSelectFile_Click(object sender, RoutedEventArgs e)
+        {
+            var ofd = new OpenFileDialog();
+            ofd.Filter = "CSV|*.csv|TXT|*.txt|Hepsi|*.*";
+            if (ofd.ShowDialog() == true)
+            {
+                txtFileName.Text = ofd.FileName;
+                TryReadFile(ofd.FileName);
+                txtEndIndex.Value = Records.Count;
+                txtEndIndex.Maximum = Records.Count;
+            }
+        }
+
+        private void TryReadFile(string fileName)
+        {
+            Records.Clear();
+            var allData = File.ReadAllLines(fileName);
+            for (int i = 0; i < allData.Length; i++)
+            {
+                var line = allData[i];
+                var lineArray = line.Split(',');
+                if (lineArray.Length >= 5)
+                {
+                    Records.Add(new Record
+                    {
+                        TimeStamp = lineArray[0],
+                        Index = Convert.ToInt32(lineArray[1]),
+                        X = Convert.ToDouble(lineArray[2]),
+                        Y = Convert.ToDouble(lineArray[3]),
+                        Z = Convert.ToDouble(lineArray[4])
+                    });
+                }
+            }
+        }
+    }
+}
---
+++ Record.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SignalDataPicker
+{
+    public class Record
+    {
+        public string TimeStamp { get; set; } = String.Empty;
+        public int Index { get; set; }
+        public double X { get; set; }
+        public double Y { get; set; }
+        public double Z { get; set; }
+    }
+}
